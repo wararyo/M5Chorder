c55c6b284283f497c695da4bfe87a2fe7457e93c
wararyo <wararyo@gmail.com>
2019-05-16 17:49:09 +0900

    Windows対応！！！

--- src/BLEMidi.h
+++ src/BLEMidi.h
@@ -5,6 +5,7 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <BLE2902.h>
 
 #define MIDI_SERVICE_UUID        "03b80e5a-ede8-4b33-a751-6ce34ec4c700"
 #define MIDI_CHARACTERISTIC_UUID "7772e5db-3868-4112-a1a9-f2669d106bf3"
